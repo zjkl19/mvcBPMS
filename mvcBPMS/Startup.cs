8aa006f62c7b30156ba845942a760d505c4b5655
zjkl19 <eamdfan@126.com>
2017-05-03 10:03:24 +0800

    first commit

---
+++ mvcBPMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvcBPMS.Startup))]
+namespace mvcBPMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
